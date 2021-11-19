09f170e80ec2f87b223d1f9176ee167296eda357
akshatmittal61 <akshatmittal2506@gmail.com>
2021-11-19 21:09:22 +0530

    Added multiple themes: light and dark

---
+++ README.md
@@ -0,0 +1,3 @@
+# Blogger
+
+Blogger is a web application built with [Node.js](https://nodejs.org/), [express](https://expressjs.com/) and [EJS](https://ejs.co/)
--- index.js
+++ index.js
@@ -7,9 +7,7 @@ const __fileName = fileURLToPath(import.meta.url);
 const __dirname = dirname(__fileName);
 const app = express();
 
-let homeContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nulla urna, finibus sed turpis ut, convallis sagittis lacus. Cras bibendum scelerisque nulla, non accumsan leo. Nulla erat arcu, feugiat sit amet interdum vel, auctor in mauris. Sed eu viverra massa, id dictum magna. Nullam cursus nibh vel pretium ullamcorper. Nunc porttitor quam posuere, interdum nisi id, scelerisque turpis. Nam nec venenatis odio";
 let aboutContent = "Vivamus sollicitudin venenatis bibendum. Ut finibus lacus enim, ac vulputate nibh feugiat vitae. Sed quis dictum risus, eu sagittis neque. Quisque tempus aliquam libero vel eleifend. Cras et quam ut magna condimentum finibus vitae in tortor. Donec elit purus, accumsan a pulvinar sit amet, cursus at urna. Etiam sit amet tellus elementum, sodales libero vitae, condimentum nulla. Proin quis lorem eget turpis hendrerit molestie.";
-let contactContent = "Proin venenatis metus eu ullamcorper venenatis. Pellentesque sollicitudin metus congue, posuere risus in, molestie metus. Phasellus congue risus libero. Praesent nec dolor bibendum, mattis libero et, venenatis urna. Vestibulum vel leo mauris. Donec lorem lorem, varius vitae nunc non, pharetra tincidunt leo. In id mi auctor, egestas magna a, dictum ex. Donec aliquet blandit lacus, sed auctor massa malesuada non. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum porttitor id ipsum in efficitur. Phasellus pharetra ornare bibendum. Curabitur condimentum eleifend lacus, sit amet rhoncus erat imperdiet id. Etiam blandit quam ac pulvinar fringilla.";
 let posts = [];
 const author = {
     name: "Akshat Mittal",
@@ -26,10 +24,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.render('home', {
-        startingContent: homeContent,
-        posts: posts
-    });
+    res.render('home', { posts: posts });
 })
 app.get('/add', (req, res) => {
     res.render('add');
